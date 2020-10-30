21c990dca25444a4a8934f527beba7e777a6053e
TheRealKevBot <Kglas001@gmail.com>
2020-10-30 10:35:52 -0600

    commit tree 20f03e4bce7c68051afe349ff0f743f1dd3beede

--- app.rb
+++ app.rb
@@ -13,4 +13,14 @@ companies = [
     Company.new('Delta', 3000)
 ]
 
-### Your code below this
+# Generate a list of strings with the name and size (eg. "Alpha - 30")
+
+# Combine all the sizes with reduce
+
+# Filter the list to show only companies over 100
+
+# Find the company named "Beta"
+
+# Find the largest company
+
+# Sort the companies from largest to smallest
